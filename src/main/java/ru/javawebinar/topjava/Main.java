d4824eb68a5b43aa65d22f7c6c4308a3ed4c457e
POTOP <t_t1@list.ru>
2017-03-22 15:02:10 +0300

    first commit

--- src/main/java/ru/javawebinar/topjava/Main.java
+++ src/main/java/ru/javawebinar/topjava/Main.java
@@ -1,11 +1,9 @@
 package ru.javawebinar.topjava;
 
 /**
- * User: gkislin
- * Date: 05.08.2015
+ * User: fatboy
+ * Date: 05.08.2025
  *
- * @link http://caloriesmng.herokuapp.com/
- * @link https://github.com/JavaOPs/topjava
  */
 public class Main {
     public static void main(String[] args) {
